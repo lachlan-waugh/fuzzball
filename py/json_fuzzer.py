bc2f92e0d9de57d8024bb3c730a21a750155e7a1
Lachlan <lachlanwaugh8@gmail.com>
2022-06-25 02:03:58 +1000

    finished adding all of the functions into the class for JSON_fuzzer, need to clean them up however.

--- py/json_fuzzer.py
+++ py/json_fuzzer.py
@@ -5,96 +5,6 @@ import json
 import random
 from helper import *
 
-# performs type swaps on ints and strings in root level of json dict
-def swap_json_values(json_object):
-    for key in json_object:
-        try:
-            json_object[key] += 1
-            json_object[key] = get_random_string(randint(2, 10))
-        except TypeError:
-            if type(json_object[key]) is dict:
-                json_object[key] = swap_json_values(json_object[key])
-            else:
-                json_object[key] = randint(2, 10)
-    return json_object
-
-def deep_nested_json(dictionary, length):
-    if length == 0:
-        return randint(0, 1024)
-    else:
-        dictionary[get_random_string(8)] = deep_nested_json({}, length - 1)
-    return dictionary
-
-def get_random_format_string(size):
-    format_string_identifiers = ["%x", "%c", "%d", "%p"]
-    payload = ""
-    for i in range(size):
-        payload += random.choice(format_string_identifiers)
-    return payload
-
-def overflow_strings_json():
-    copy = json_input.copy()
-    for i in range(1000, 12000, 200):
-        for key in copy.keys():
-            try:
-                copy[key] += 1
-                copy[key] -= 1
-            except TypeError:
-                if type(copy[key]) is str:
-                    copy[key] = get_random_string(i)
-        payload = json.dumps(copy).encode("UTF-8")
-        test_payload(binary, payload)
-
-def overflow_integers_json():
-    keys = list(json_input.keys())
-    for i in range(len(keys)):
-        copy = json_input.copy()
-        try:
-            copy[keys[i]] += 1
-            copy[keys[i]] = 429496729
-        except TypeError:
-            continue
-        payload = json.dumps(copy).encode("UTF-8")
-        test_payload(binary, payload)
-    copy = json_input.copy()
-    for key in copy.keys():
-        try:
-            copy[key] += 1
-            copy[key] = 429496729
-        except TypeError:
-            continue
-    payload = json.dumps(copy).encode("UTF-8")
-    test_payload(binary, payload)
-
-def format_string_fuzz():
-    copy = json_input.copy()
-    for key in copy.keys():
-        if type(copy[key]) is str:
-            copy[key] = get_random_format_string(64)
-        elif type(copy[key]) is int:
-            copy[key] = 429496730
-    payload = json.dumps(copy).encode("UTF-8")
-    test_payload(binary, payload)
-
-def random_types():
-    actions = {
-        "string": get_random_string(randint(16, 12000)),
-        "boolean": random.choice([True, False]),
-        "int": randint(-429496729, 429496729),
-        "none": None,
-        "list": list(range(randint(-64, 0), randint(0, 64))),
-        "float": random.uniform(-128, 128),
-        "dict": random_json(False)
-    }
-
-    for i in range(100):
-        payload = json_input.copy()
-        for key in payload.keys():
-            choice = random.choice(actions.keys)
-            payload(key) = actions[choice]
-
-    return json.dumps(payload).encode('UTF-8')
-
 class JSONFuzzer:
     def __init__(self, input):
         try:
@@ -113,6 +23,7 @@ class JSONFuzzer:
 
         return json.dumps(d).encode('UTF-8')
 
+    # ASDASD
     def nullify_json(self):
         payload = self._json.copy()
         # set inputs to 0 equivelants
@@ -129,15 +40,7 @@ class JSONFuzzer:
         payload[key] = None for key in payload.keys()
         return json.dumps(payload).encode("UTF-8")
 
-    def remove_fields(self):
-        for i in range(len(self._json.keys())):
-            payload = json_object.copy()
-            for x in range(i):
-                # have chosen not to sort to have different subsets of fields removed (more random impact ?)
-                del payload[list(self._json.keys())[x]]
-
-            yield json.dumps(payload).encode("UTF-8")
-
+    # asdasdasdas
     def add_fields(self):
         # add additional entries
         for i in range(25):
@@ -147,16 +50,115 @@ class JSONFuzzer:
 
             yield json.dumps(payload).encode("UTF-8")
 
-    def swap_json_fields():
+    def remove_fields(self):
+        for i in range(len(self._json.keys())):
+            payload = json_object.copy()
+            for x in range(i):
+                # have chosen not to sort to have different subsets of fields removed (more random impact ?)
+                del payload[list(self._json.keys())[x]]
+
+            yield json.dumps(payload).encode("UTF-8")
+
+    def swap_json_fields(self):
         fields = [ self._json[entry] for entry in self._json ]
-        payload = json_input.copy()
+        payload = self._json.copy()
         for entry in payload:
             payload[entry] = random.choice(fields)
 
         return json.dumps(payload).encode("UTF-8")
 
-    def wrong_type_values_json():
+    # ADASDSA
+    def wrong_type_values_json(self):
         return self._json.copy() + json.dumps(swap_json_values(self._json.copy())).encode("UTF-8")
+
+    # TODO: fix the actions switch
+    def random_types(self):
+        actions = {
+            "string": get_random_string(randint(16, 12000)),
+            "boolean": random.choice([True, False]),
+            "int": randint(-429496729, 429496729),
+            "none": None,
+            "list": list(range(randint(-64, 0), randint(0, 64))),
+            "float": random.uniform(-128, 128),
+            "dict": random_json(False)
+        }
+
+        for i in range(100):
+            payload = self._json.copy()
+            for key in payload.keys():
+                choice = random.choice(actions.keys)
+                payload(key) = actions[choice]
+
+        return json.dumps(payload).encode('UTF-8')
+
+    # ASDASDASASDS
+    # performs type swaps on ints and strings in root level of json dict
+    def swap_json_values(self):
+        for key in self._json:
+            try:
+                self._json[key] += 1
+                self._json[key] = get_random_string(randint(2, 10))
+            except TypeError:
+                if type(self._json[key]) is dict:
+                    self._json[key] = swap_json_values(self._json[key])
+                else:
+                    self._json[key] = randint(2, 10)
+        return self._json
+
+    def deep_nested_json(dictionary, length):
+        if length == 0:
+            return randint(0, 1024)
+        else:
+            dictionary[get_random_string(8)] = deep_nested_json({}, length - 1)
+
+        return dictionary
+
+    # TODO: fix
+    def get_random_format_string(size):
+        return ''.join(random.choice(['%x', '%c', '%d', '%p']) for _ in range(size))
+
+    def overflow_strings_json():
+        payload = json_input.copy()
+        for i in range(1000, 12000, 200):
+            for key in payload.keys():
+                try:
+                    payload[key] += 1
+                    payload[key] -= 1
+                except TypeError:
+                    if type(payload[key]) is str:
+                        payload[key] = get_random_string(i)
+            return json.dumps(payload).encode("UTF-8")
+
+    def integer_overflow_keys():
+        keys = list(json_input.keys())
+        for i in range(len(keys)):
+            payload = json_input.copy()
+            try:
+                payload[keys[i]] += 1
+                payload[keys[i]] = 429496729
+            except TypeError:
+                continue
+            return json.dumps(payload).encode("UTF-8")
+
+    def integer_overflow_values():
+        payload = json_input.copy()
+        for key in copy.keys():
+            try:
+                payload[key] += 1
+                payload[key] = 429496729
+            except TypeError:
+                continue
+        return json.dumps(payload).encode("UTF-8")
+
+    def format_string_fuzz(self):
+        payload = self._json.copy()
+        for key in payload.keys():
+            if type(payload[key]) is str:
+                payload[key] = get_random_format_string(64)
+            elif type(copy[key]) is int:
+                payload[key] = 429496730
+
+        return json.dumps(copy).encode("UTF-8")
 
     def generate_input(self):
         ##########################################################
