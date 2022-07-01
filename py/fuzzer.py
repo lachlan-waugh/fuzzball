b8240c6fa7114e319dc97b860446ddc11c0cc9f0
Lachlan <lachlanwaugh8@gmail.com>
2022-07-01 20:00:13 +1000

    modified the execution script to actually run the fuzzer.

--- py/fuzzer.py
+++ py/fuzzer.py
@@ -4,30 +4,32 @@ import sys
 import os
 import logging
 
-# context.log_level = 'ERROR'
+"""
+    $1 = binary name
+    $2 = sampleinput
+"""
+def fuzzer():
+    if len(sys.argv) != 3:
+        sys.exit("Usage: python3 fuzzer.py [binaryName] [sampleInput]")
 
-# argument error checking
-# 1 = binary name
-# 2 = sampleinput
+    binary_file, sample_input = sys.argv[1:3]
+    if not (os.path.isfile(binary_file)):
+        sys.exit(f'[x] ERROR: binary file \'{os.path.basename(binary_file)}\' not found')
+    elif not (os.path.isfile(sample_input)):
+        sys.exit(f'[x] ERROR: sample input \'{os.path.basename(sample_input)}\' not found.')
+    else:
+        print(f'[*] binary file: {os.path.basename(binary_file)}\n[*] sample input: {os.path.basename(sample_input)}')
 
-if len(sys.argv) != 3:
-    sys.exit("Usage: python3 fuzzer.py [binaryName] [sampleInput]")
+    # first test some basic input, that doesn't rely on the sample
+    generate_inputs(binary_file, simple_fuzz())
 
-binary_file, sample_input = sys.argv[1:3]
-if not (os.path.isfile(binary_file)):
-    sys.exit(f'[x] ERROR: binary file \'{os.path.basename(binary_file)}\' not found')
-elif not (os.path.isfile(sample_input)):
-    sys.exit(f'[x] ERROR: sample input \'{os.path.basename(sample_input)}\' not found.')
-else:
-    print(f'[*] binary file: {os.path.basename(binary_file)}\n[*] sample input: {os.path.basename(sample_input)}')
+    # next, mutate the sample input
+    with open(sample_input) as input:
+        generate_inputs(binary_file, get_fuzzer(input).generate_input())
 
-# first test some basic input, that doesn't rely on the sample
-generate_inputs(binary_file, simple_fuzz())
+    # busy wait until the workers finish
+    while len(MP.active_children()) > 0:
+        sleep(1)
 
-# next, mutate the sample input
-with open(sample_input) as input:
-    generate_inputs(binary_file, get_fuzzer(input).generate_input())
-
-# busy wait until the workers finish
-while len(MP.active_children()) > 0:
-    sleep(1)
+if __name__ == "__main__":
+    fuzzer()
