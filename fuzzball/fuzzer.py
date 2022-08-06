ee3d631aec3a05f72a37e834f80cd5f14571a907
Lachlan <lachlanwaugh8@gmail.com>
2022-08-06 13:53:20 +1000

    initialiased the coverage module to track code paths.

--- fuzzball/fuzzer.py
+++ fuzzball/fuzzer.py
@@ -1,7 +1,7 @@
 from .modules.helper import *
 from .modules.harness import *
 from .modules.bootstrap import *
-
+from .coverage.coverage import *
 """
 > $1: binary name
 > $2: sampleinput
@@ -11,6 +11,7 @@ class Fuzzer:
         self._binary_file, self._sample_input = get_arguments()
         self._harness = Harness(self._binary_file)
         self._strategies = Bootstrap(self._sample_input)
+        self._coverage = Coverage()
 
     def run(self):
         # first generate some input, that doesn't rely on the sample
